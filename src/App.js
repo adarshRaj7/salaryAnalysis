9a188ee30bcc1449da99ac2d090ee492b5b18f55
Adarsh Raj <adarshrj62@gmail.com>
2024-05-22 13:26:09 +0530

    Table structure added

--- src/App.js
+++ src/App.js
@@ -1,40 +1,135 @@
-import logo from "./logo.svg";
+import { Layout, Space, Table, Tag, theme } from "antd";
 import "./App.css";
-import { parseCsv } from "./services/parseCsv/parseCsv";
-import { useState } from "react";
 import { yearwiseData } from "./services/salaries/yearwiseData";
+import { Content } from "antd/es/layout/layout";
+import { useEffect } from "react";
 
 //csv
 
-function App() {
-  yearwiseData();
-  // let data = [];
-  // parseCsv()
-  // .then((results) => {
-  //   data = results;
-  //   console.log(data);
-  // })
-  // .catch((error) => {
-  //   console.log(error);
-  // });
-  // console.log("data", data);
+ function App() {
+  const  yearwise  = yearwiseData();
+  console.log("In app",yearwise)
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  //table test
+
+  const columns = [
+    {
+      title: "Year",
+      dataIndex: "year",
+      showSorterTooltip: {
+        target: "full-header",
+      },
+      filters: [
+        {
+          text: "2024",
+          value: 2024,
+        },
+        {
+          text: "2023",
+          value: 2023,
+        },
+        {
+          text: "2022",
+          value: 2022,
+        },
+        {
+          text: "2021",
+          value: 2021,
+        },
+        {
+          text: "2020",
+          value: 2020,
+        },
+      ],
+      // specify the condition of filtering result
+      // here is that finding the name started with `value`
+      onFilter: (value, record) => record.year.indexOf(value) === 0,
+      sorter: (a, b) => a.year - b.year,
+    },
+    {
+      title: "Total Jobs",
+      dataIndex: "jobs",
+      defaultSortOrder: "descend",
+      sorter: (a, b) => a.jobs - b.jobs,
+    },
+    {
+      title: "Average Salary (USD)",
+      dataIndex: "salary",
+      sorter: (a, b) => a.salary - b.salary,
+
+      // filters: [
+      //   {
+      //     text: "London",
+      //     value: "London",
+      //   },
+      //   {
+      //     text: "New York",
+      //     value: "New York",
+      //   },
+      // ],
+      // onFilter: (value, record) => record.address.indexOf(value) === 0,
+    },
+  ];
+  const data = [
+    {
+      key: "1",
+      year: "2022",
+      jobs: 31,
+      salary: 1000000,
+    },
+    {
+      key: "2",
+      year: "2021",
+      jobs: 42,
+      salary: 2000000,
+    },
+    {
+      key: "3",
+      year: "2024",
+      jobs: 32,
+      salary: 3000000,
+    },
+    {
+      key: "4",
+      year: "2023",
+      jobs: 33,
+      salary: 4000000,
+    },
+    {
+      key: "5",
+      year: "2020",
+      jobs: 34,
+      salary: 5000000,
+    },
+  ];
+  const onChange = (pagination, filters, sorter, extra) => {
+    console.log("params", pagination, filters, sorter, extra);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{height:"100vh"}}>
+      <Content style={{ padding: "12px 48px" }}>
+        <div style={{
+            background: colorBgContainer,
+            minHeight: 280,
+            padding: 24,
+            borderRadius: borderRadiusLG,
+          }}>
+            Main Table
+          <Table
+            columns={columns}
+            dataSource={data}
+            onChange={onChange}
+            showSorterTooltip={{
+              target: "sorter-icon",
+            }}
+          />
+        </div>
+      </Content>
+    </Layout>
   );
 }
 
--- src/services/salaries/yearwiseData.js
+++ src/services/salaries/yearwiseData.js
@@ -4,17 +4,28 @@ export const yearwiseData = () => {
   parseCsv()
     .then((results) => {
       let data = results.data;
-      const yearwise = {}
-      // console.log("data from year", data);
-      let count = 0;
+      const yearwise = {
+        2023: [],
+      };
       data.forEach((element) => {
-        console.log("element " + count, element);
-        count++;
-        let year = element.;
+        let year = element.work_year;
+        if (yearwise[year]) {
+          yearwise[year].push(element);
+        } else {
+          yearwise[year] = [element];
+        }
       });
-      // console.log("yearwise",yearwise)
+      // console.log("2020", yearwise[2020].length);
+      // console.log("2021", yearwise[2021].length);
+      // console.log("2022", yearwise[2022].length);
+      // console.log("2023", yearwise[2023].length);
+      // console.log("2024", yearwise[2024].length);
+      // console.log("total", yearwise[2024].length+yearwise[2023].length+yearwise[2022].length+yearwise[2021].length+yearwise[2020].length);
+      console.log("yearwise",yearwise)
+      return yearwise;
     })
     .catch((error) => {
       console.log(error);
+      return error;
     });
 };
